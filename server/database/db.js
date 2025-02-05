399d59e22947305a762277d7dbbbd9b62c319648
Matheo <matheoprofessionnel12@gmail.com>
2025-02-05 15:17:04 +0100

    update mongo info

--- server/database/db.js
+++ server/database/db.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const USERNAME=process.env.DB_USERNAME;
 const PASSWORD=process.env.DB_PASSWORD;
+
 const Connection = async () => {
-    const URL=`mongodb://${USERNAME}:${PASSWORD}@ac-lc1tmha-shard-00-00.m5ssllm.mongodb.net:27017,ac-lc1tmha-shard-00-01.m5ssllm.mongodb.net:27017,ac-lc1tmha-shard-00-02.m5ssllm.mongodb.net:27017/?ssl=true&replicaSet=atlas-3lgp0k-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const URL=`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.gwz6y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true });
         console.log('Database connect successfully');
--- server/utils/upload.js
+++ server/utils/upload.js
@@ -8,7 +8,7 @@ const USERNAME=process.env.DB_USERNAME;
 const PASSWORD=process.env.DB_PASSWORD;
 
 const storage = new GridFsStorage({
-    url: `mongodb://${USERNAME}:${PASSWORD}@ac-lc1tmha-shard-00-00.m5ssllm.mongodb.net:27017,ac-lc1tmha-shard-00-01.m5ssllm.mongodb.net:27017,ac-lc1tmha-shard-00-02.m5ssllm.mongodb.net:27017/?ssl=true&replicaSet=atlas-3lgp0k-shard-0&authSource=admin&retryWrites=true&w=majority`, 
+    url: `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.gwz6y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, 
     options:{useUnifiedTopology:true, useNewUrlParser:true},
     file:(request,file)=>{
         const match = ["image/png", "image/jpg"];
